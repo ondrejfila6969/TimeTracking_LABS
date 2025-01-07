b7c2353acb8c20759d02272cccc0a8ab16080dc7
Ondřej Fíla <114986357+ondrejfila6969@users.noreply.github.com>
2025-01-07 16:48:52 +0100

    Update

--- TimeTrackingC#/obj/Debug/net8.0/TimeTrackingLABS.AssemblyInfo.cs
+++ TimeTrackingC#/obj/Debug/net8.0/TimeTrackingLABS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TimeTrackingLABS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27f9727710e8ef4f600362f29e67f184b8374aa9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c298d1c650172f2f3e218fa1253ab74777eba51")]
 [assembly: System.Reflection.AssemblyProductAttribute("TimeTrackingLABS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TimeTrackingLABS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
